f4d1e3694f1a9c46339a23f88e2f1d277c5af095
Peter Reutemann <fracpete@gmail.com>
2021-01-13 11:08:59 +1300

    simple loader for matrices

--- src/main/java/weka/core/converters/MatlabMatLoader.java
+++ src/main/java/weka/core/converters/MatlabMatLoader.java
@@ -21,8 +21,18 @@
 
 package weka.core.converters;
 
+import us.hebi.matlab.mat.format.Mat5;
+import us.hebi.matlab.mat.format.Mat5File;
+import us.hebi.matlab.mat.types.AbstractCharBase;
+import us.hebi.matlab.mat.types.Cell;
+import us.hebi.matlab.mat.types.Char;
+import us.hebi.matlab.mat.types.MatFile;
+import us.hebi.matlab.mat.types.Matrix;
+import weka.core.Attribute;
+import weka.core.DenseInstance;
 import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.Option;
 import weka.core.OptionHandler;
 import weka.core.RevisionUtils;
 import weka.core.Utils;
@@ -32,10 +42,14 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.Vector;
 import java.util.zip.GZIPInputStream;
 
@@ -55,6 +69,21 @@ public class MatlabMatLoader
   /** the default file extension */
   public final static String FILE_EXTENSION = ".mat";
 
+  /** the default entry name. */
+  public final static String DEFAULT_ENTRY_NAME = "";
+
+  /** the name of the entry to retrieve. */
+  protected String m_EntryName = DEFAULT_ENTRY_NAME;
+
+  /** the default max number of values for nominal attributes. */
+  public final static int DEFAULT_MAX_NOMINAL_VALUES = 25;
+
+  /** the maximum number of values for nominal attributes. */
+  protected int m_MaxNominalValues = DEFAULT_MAX_NOMINAL_VALUES;
+
+  /** the loaded data. */
+  protected Instances m_Data;
+
   /**
    * Returns a string describing this Loader
    *
@@ -63,6 +92,66 @@ public class MatlabMatLoader
    */
   public String globalInfo() {
     return "Reads binary Matlab .mat files.";
+  }
+
+  /**
+   * Sets the entry to retrieve; first if empty.
+   *
+   * @param value	the name
+   */
+  public void setEntryName(String value) {
+    m_EntryName = value;
+  }
+
+  /**
+   * Returns the entry name to retrieve; first if empty.
+   *
+   * @return		the name
+   */
+  public String getEntryName() {
+    return m_EntryName;
+  }
+
+  /**
+   * Returns the tip text for this property
+   *
+   * @return tip text for this property suitable for displaying in the
+   *         explorer/experimenter gui
+   */
+  public String entryNameTipText() {
+    return "The entry name to retrieve; first if empty.";
+  }
+
+  /**
+   * Sets the maximum number of values for nominal attributes.
+   *
+   * @param value	the maximum; -1 for always nominal, 0 for always string
+   */
+  public void setMaxNominalValues(int value) {
+    if (value < -1)
+      value = -1;
+    m_MaxNominalValues = value;
+  }
+
+  /**
+   * Returns the maximum number of values for nominal attributes.
+   *
+   * @return		the maximum; -1 for always nominal, 0 for always string
+   */
+  public int getMaxNominalValues() {
+    return m_MaxNominalValues;
+  }
+
+  /**
+   * Returns the tip text for this property
+   *
+   * @return tip text for this property suitable for displaying in the
+   *         explorer/experimenter gui
+   */
+  public String maxNominalValuesTipText() {
+    return "The maximum number of values for nominal attributes, beyond that "
+      + "it is considered a string attribute; use -1 to always convert to "
+      + "nominal and 0 to always convert to string.";
   }
 
   /**
@@ -100,7 +189,15 @@ public class MatlabMatLoader
   public Enumeration listOptions() {
     Vector result = new Vector();
 
-    // TODO
+    result.addElement(new Option("\tThe entry name to retrieve; first if empty\n"
+      + "\t(default: " + DEFAULT_ENTRY_NAME + ")",
+      "entry-name", 1, "-entry-name <name>"));
+
+    result.addElement(new Option("\tThe maximum number of distinct values a NOMINAL attribute\n"
+      + "\tcan have; beyond that it is considered a STRING attribute.\n"
+      + "\tUse -1 to always convert to NOMINAL, 0 to always convert to STRING.\n"
+      + "\t(default: " + DEFAULT_MAX_NOMINAL_VALUES + ")",
+      "max-nominal-values", 1, "-max-nominal-values <int>"));
 
     return result.elements();
   }
@@ -115,7 +212,17 @@ public class MatlabMatLoader
   public void setOptions(String[] options) throws Exception {
     String 	tmp;
 
-    // TODO
+    tmp = Utils.getOption("entry-name", options);
+    if (!tmp.isEmpty())
+      setEntryName(tmp);
+    else
+      setEntryName(DEFAULT_ENTRY_NAME);
+
+    tmp = Utils.getOption("max-nominal-values", options);
+    if (!tmp.isEmpty())
+      setMaxNominalValues(Integer.parseInt(tmp));
+    else
+      setMaxNominalValues(DEFAULT_MAX_NOMINAL_VALUES);
 
     Utils.checkForRemainingOptions(options);
   }
@@ -130,7 +237,11 @@ public class MatlabMatLoader
 
     result = new ArrayList<String>();
 
-    // TODO
+    result.add("-entry-name");
+    result.add(getEntryName());
+
+    result.add("-max-nominal-values");
+    result.add("" + getMaxNominalValues());
 
     return result.toArray(new String[0]);
   }
@@ -142,6 +253,7 @@ public class MatlabMatLoader
    */
   public void reset() throws IOException {
     m_structure = null;
+    m_Data      = null;
 
     setRetrieval(NONE);
 
@@ -158,6 +270,7 @@ public class MatlabMatLoader
    */
   public void setSource(File file) throws IOException {
     m_structure = null;
+    m_Data      = null;
 
     setRetrieval(NONE);
 
@@ -176,19 +289,6 @@ public class MatlabMatLoader
 
     m_sourceFile = file;
     m_File       = file.getAbsolutePath();
-  }
-
-  /**
-   * Resets the Loader object and sets the source of the data set to be 
-   * the supplied url.
-   *
-   * @param url 	the source url.
-   * @throws IOException        if an error occurs
-   */
-  public void setSource(URL url) throws IOException {
-    m_structure = null;
-    setRetrieval(NONE);
-    setSource(url.openStream());
   }
 
   /**
@@ -228,6 +328,125 @@ public class MatlabMatLoader
   }
 
   /**
+   * Performs the actual conversion.
+   *
+   * @param array	the array to convert
+   * @throws IOException if something goes wrong with the conversion
+   * @return the converted data
+   */
+  protected Instances convert(us.hebi.matlab.mat.types.Array array) throws IOException {
+    Matrix			matrix;
+    Char 			matChar;
+    Cell 			matCell;
+    int				i;
+    int				n;
+    Object[][]			data;
+    boolean[]			numeric;
+    ArrayList<Attribute>	atts;
+    double[]			values;
+    String			cell;
+    Map<Integer,Set<String>> 	unique;
+    List<String>		labels;
+
+    if (array.getNumDimensions() > 2)
+      throw new IllegalStateException("Cannot handle arrays with more than two dimensions, received: " + array.getNumDimensions());
+    matrix = null;
+    if (array instanceof Matrix)
+      matrix = (Matrix) array;
+    matCell = null;
+    if (array instanceof Cell)
+      matCell = (Cell) array;
+    matChar = null;
+    if (array instanceof Char)
+      matChar = (Char) array;
+    if ((matrix == null) && (matCell == null) && (matChar == null))
+      throw new IllegalStateException("Unhandled array type: " + array.getClass().getName());
+
+    // convert matrix
+    data = new Object[array.getNumRows()][array.getNumCols()];
+    numeric = new boolean[array.getNumCols()];
+    for (i = 0; i < numeric.length; i++)
+      numeric[i] = true;
+    for (n = 0; n < array.getNumRows(); n++) {
+      for (i = 0; i < array.getNumCols(); i++) {
+        if (matrix != null) {
+	  data[n][i] = matrix.getDouble(n, i);
+	}
+        else if (matCell != null) {
+          if (matCell.get(n, i) instanceof AbstractCharBase) {
+	    data[n][i] = ((AbstractCharBase) matCell.get(n, i)).asCharSequence().toString();
+	    numeric[i] = false;
+	  }
+          else {
+            cell = matCell.get(n, i).toString();
+            try {
+              data[n][i] = Double.parseDouble(cell);
+	    }
+	    catch (Exception e) {
+              data[n][i] = cell;
+	      numeric[i] = false;
+	    }
+	  }
+	}
+        else if (matChar != null) {
+	  data[n][i] = matChar.getChar(n, i);
+          numeric[i] = false;
+	}
+      }
+    }
+
+    // determine unique values
+    unique = new HashMap<Integer, Set<String>>();
+    for (i = 0; i < array.getNumCols(); i++) {
+      if (!numeric[i]) {
+	for (n = 0; n < array.getNumRows(); n++) {
+	  if (!unique.containsKey(i))
+	    unique.put(i, new HashSet<String>());
+	  unique.get(i).add((String) data[n][i]);
+	}
+      }
+    }
+
+    // create dataset
+    // 1. header
+    atts = new ArrayList<Attribute>();
+    for (i = 0; i < numeric.length; i++) {
+      if (numeric[i]) {
+	atts.add(new Attribute("col-" + (i + 1)));
+      }
+      else {
+        // nominal
+        if ((m_MaxNominalValues == -1) || (unique.get(i).size() <= m_MaxNominalValues)) {
+          labels = new ArrayList<String>(unique.get(i));
+          Collections.sort(labels);
+	  atts.add(new Attribute("col-" + (i + 1), labels));
+	}
+        // string
+        else {
+	  atts.add(new Attribute("col-" + (i + 1), (List<String>) null));
+	}
+      }
+    }
+    m_Data = new Instances(retrieveFile().getName(), atts, array.getNumRows());
+
+    // 2. data
+    for (n = 0; n < array.getNumRows(); n++) {
+      values = new double[m_Data.numAttributes()];
+      for (i = 0; i < array.getNumCols(); i++) {
+        if (m_Data.attribute(i).isNumeric())
+          values[i] = (Double) data[n][i];
+        else if (m_Data.attribute(i).isNominal())
+          values[i] = m_Data.attribute(i).indexOfValue((String) data[n][i]);
+        else
+          values[i] = m_Data.attribute(i).addStringValue((String) data[n][i]);
+      }
+      m_Data.add(new DenseInstance(1.0, values));
+    }
+
+    return m_Data;
+  }
+
+  /**
    * Return the full data set. If the structure hasn't yet been determined
    * by a call to getStructure then method should do so before processing
    * the rest of the data set.
@@ -237,18 +456,32 @@ public class MatlabMatLoader
    * @throws IOException        if there is no source or parsing fails
    */
   public Instances getDataSet() throws IOException {
-    Instance		inst;
+    Mat5File				mat5;
+    us.hebi.matlab.mat.types.Array	array;
+    int					i;
 
     if (getRetrieval() == INCREMENTAL)
       throw new IOException("Cannot mix getting Instances in both incremental and batch modes");
 
     setRetrieval(BATCH);
-    if (m_structure == null)
-      getStructure();
 
-    // TODO
+    mat5  = Mat5.readFromFile(retrieveFile());
+    array = null;
+    i     = 0;
+    System.err.println("Entries in: " + retrieveFile());
+    for (MatFile.Entry entry: mat5.getEntries()) {
+      System.err.println((i+1) + ": " + entry.getName());
+      if (entry.getName().equals(m_EntryName) || (m_EntryName.isEmpty() && (array == null)))
+        array = entry.getValue();
+      i++;
+    }
+    if (array == null)
+      throw new IOException("Failed to load array with name: " + m_EntryName);
 
-    return null;
+    m_Data = convert(array);
+    m_structure = new Instances(m_Data, 0);
+
+    return m_Data;
   }
 
   /**
